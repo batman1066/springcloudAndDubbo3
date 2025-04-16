a4bf4836c86020924036b67eab0f907f0adbc655
yangming <1066537334@qq.com>
2025-04-16 20:57:04 +0800

    集成sentinel

--- web/src/main/java/org/example/web/controller/TestController.java
+++ web/src/main/java/org/example/web/controller/TestController.java
@@ -1,10 +1,11 @@
 package org.example.web.controller;
 
 import lombok.extern.slf4j.Slf4j;
-//import org.apache.dubbo.config.annotation.DubboReference;
-//import org.example.accountapi.AccountApi;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.example.accountapi.AccountApi;
+import org.example.bussinessapi.BussinessApi;
+import org.example.bussinessapi.dto.PurchaseDTO;
+import org.example.mvc.wrapper.RestWrapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,13 +20,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/test")
 @Slf4j
+@RestWrapper
 public class TestController {
-    @DubboReference
+    @DubboReference(retries = 0, timeout = 1000)
+    BussinessApi bussinessApi;
+    @DubboReference(retries = 0, timeout = 1000)
     AccountApi accountApi;
-    @GetMapping("/test")
-    public String test(@RequestParam("hehe") String hehe){
-        log.info("rest api hehe:{}",hehe);
-        return accountApi.test(hehe);
 
+    @GetMapping("/test")
+    public String test(@RequestParam("hehe") String hehe) throws InterruptedException {
+        log.info("rest api hehe:{}", hehe);
+        //Thread.sleep(500);
+        return accountApi.test(hehe);
+        //return "这是web,hehe:" + hehe;
+    }
+
+    @GetMapping("/purchase")
+    public String purchase(@RequestParam(value = "userId", defaultValue = "1") String userId,
+                           @RequestParam(value = "commodityCode", defaultValue = "001") String commodityCode,
+                           @RequestParam(value = "orderCount", defaultValue = "1") Integer orderCount,
+                           @RequestParam(value = "transactionMode", defaultValue = "AT") String transactionMode) {
+        PurchaseDTO purchaseDTO = new PurchaseDTO();
+        purchaseDTO.setUserId(userId);
+        purchaseDTO.setCommodityCode(commodityCode);
+        purchaseDTO.setOrderCount(orderCount);
+        purchaseDTO.setTransactionMode(transactionMode);
+        log.info("rest api purchaseDTO:{}", purchaseDTO);
+        return bussinessApi.purchase(purchaseDTO);
     }
 }
